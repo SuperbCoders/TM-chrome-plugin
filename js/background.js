ad7e7fde5e938b59ed4b5ca22938ce82a954cf99
plotnikovVV <4.plotnikov@gmail.com>
2020-10-10 17:43:54 +0300

    add func setTimeSite

--- js/background.js
+++ js/background.js
@@ -12,12 +12,24 @@
 //   });
 // Called when the user clicks on the browser action.
 
+
+function setTimeSite(url, time) {
+
+
+
+  chrome.storage.local.set({key: value}, function() {
+    console.log('Value is set to ' + value);
+  });
+}
+
 function getDomainFromURL(url) {
   u = new URL(url)
   console.log('Host is:', u.host)
+
   host_words = u.host.split(".")
-  host_words_reverse = host_words.reverse()
   console.log('host_words:', host_words)
+
+  host_words_reverse = host_words.reverse()
   console.log('host_words_reverse:', host_words_reverse)
 
   let domain = "".concat(host_words_reverse[1], ".", host_words_reverse[0])
@@ -44,6 +56,8 @@ chrome.tabs.onActivated.addListener(function(tab) {
         // seconds = new Date() / 1000
         seconds = Date.now()
         console.log('timestamp: ', seconds)
+
+        // setTimeSite(tabInfo.url, )
     })
 })
 
